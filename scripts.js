19ee0b094225596d46fcbec93d0fca85ba267e1c
Evgenyi_Bartalevich <bartalyukevgen@mail.ru>
2021-12-16 02:14:37 +0300

    First commit dz15

---
+++ scripts.js
@@ -0,0 +1,90 @@
+// Задание - 1
+
+/*console.log('/-------от 1 до 50-------/');
+
+let p = 1;
+while(p <= 50) {
+    console.log(p);
+    p++;
+}
+
+console.log('/------от 35 до 8------/');
+
+let i = 35;
+while(i > 7){
+    console.log(i);
+    i--;
+}*/
+
+// Задание - 2
+
+/*let h = 89;
+let
+while(h > 10){
+    console.log(h + '<br>');
+    h--;
+}*/
+
+// Задание - 3
+
+/*let f = 100;
+let sum = 0;
+for (let y = 0; y <= f; y++){
+    console.log(y);
+    sum += y;
+}
+console.log('/-------Сумма чисел от 0 до 100-------/');
+console.log(sum);*/
+
+// Задание - 4
+
+/*let t = 5;
+let sum = 0;
+for (let g = 1; g <= t; g++){
+    sum = 0;
+    for(let v = 1; v <= g; v++){
+        sum += v;
+    }
+    console.log('Sum ' + g + ' = ' + sum);
+}*/
+
+// Задание 5
+
+/*console.log('/---------while-----------/');
+let b = 8;
+    while(b <= 56) {
+        console.log(b);
+        b += 2; //b++;
+}*/
+
+/*console.log('/---------for-----------/');
+for (let r = 8; r <= 56; r++) {
+    if(r % 2 !== 0) continue;
+    console.log(r);
+}*/
+
+// Задание 6  
+
+/*for (let t = 2; t <= 10; t++){ 
+
+    for (let u = t; u <= 10; u++) { 
+        
+        console.log(t + ' * ' + u + ' = ' + ( t * u ))   
+    }  
+
+}*/
+
+// Задание 7
+
+/*let n = 1000;
+let itr = 0;
+while (n > 50) {
+    n = n / 2;
+    itr++;
+    if (n < 50) break;
+    console.log(n, ' итерация ' + itr);
+} */
+// Полученное число 62.5
+
+
+
